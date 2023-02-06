daf4c44839c76afc8537cf88ea46179613aaab18
0Suphan0 <80632509+0Suphan0@users.noreply.github.com>
2023-02-07 00:45:38 +0300

    Created layers.

---
+++ Core/ETicaretAPI.Domain/Enitites/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaretAPI.Domain.Enitites.Common;
+
+namespace ETicaretAPI.Domain.Enitites
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public long Price { get; set; }
+
+    }
+}
