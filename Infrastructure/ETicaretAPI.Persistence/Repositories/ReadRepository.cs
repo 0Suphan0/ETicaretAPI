df0af9f4e04e023e320caa44d23349cb387066b7
0Suphan0 <80632509+0Suphan0@users.noreply.github.com>
2023-02-27 21:35:28 +0300

    Tracking Optimization done.

--- Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/ReadRepository.cs
@@ -25,15 +25,57 @@ namespace ETicaretAPI.Persistence.Repositories
         public DbSet<T> Table => _context.Set<T>();
 
 
-        public IQueryable<T> GetAll() => Table;
+        public IQueryable<T> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+
+            if (!tracking)
+            {
+                query = query.AsNoTracking();
+                
+            }
+            return query;
+        }
 
 
-        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method) => Table.Where(method);
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> method,bool tracking= true)
+        {
+            var query = Table.Where(method);
+
+            if (!tracking)
+            {
+                query = query.AsNoTracking();
+
+            }
+            return query;
+        }
 
 
-        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method) => await Table.FirstOrDefaultAsync(method);
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> method, bool tracking = true)
+        {
+            //=> await Table.FirstOrDefaultAsync(method);
 
-        public async Task<T> GetByIdAsync(string id) => await Table.FindAsync(Guid.Parse(id));
+            var query = Table.AsQueryable();
+            if (!tracking)
+            {
+                query=Table.AsNoTracking();
+            }
+
+            return await query.FirstOrDefaultAsync(method);
+        }
+
+        public async Task<T> GetByIdAsync(string id, bool tracking = true)
+        {
+            //=> await Table.FindAsync(Guid.Parse(id));
+
+            var query = Table.AsQueryable();
+            if (!tracking)
+            {
+                query = Table.AsNoTracking();
+            }
+
+            return await query.FirstOrDefaultAsync(p => p.Id == Guid.Parse(id));
+        }
         //marker design pattern
         // await Table.FirstOrDefaultAsync(p => p.Id == Guid.Parse(id));
     }
--- Presentation/ETicaretAPI.API/Controllers/ProductController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -21,21 +21,25 @@ namespace ETicaretAPI.API.Controllers
         [HttpGet]
         public async Task Get()
         {
-           await _productWriteRepository.AddRangeAsync(new()
-            {
-                new Product
-                {
-                    Id = Guid.NewGuid(), Name = "Product 1", CreatedTime = DateTime.UtcNow, Price = 100, Stock = 5
-                },
-                new Product
-                {
-                    Id = Guid.NewGuid(), Name = "Product 2", CreatedTime = DateTime.UtcNow, Price = 200, Stock = 15
-                },
-                new Product
-                {
-                    Id = Guid.NewGuid(), Name = "Product 3", CreatedTime = DateTime.UtcNow, Price = 300, Stock = 25
-                },
-            });
+           //await _productWriteRepository.AddRangeAsync(new()
+           // {
+           //     new Product
+           //     {
+           //         Id = Guid.NewGuid(), Name = "Product 1", CreatedTime = DateTime.UtcNow, Price = 100, Stock = 5
+           //     },
+           //     new Product
+           //     {
+           //         Id = Guid.NewGuid(), Name = "Product 2", CreatedTime = DateTime.UtcNow, Price = 200, Stock = 15
+           //     },
+           //     new Product
+           //     {
+           //         Id = Guid.NewGuid(), Name = "Product 3", CreatedTime = DateTime.UtcNow, Price = 300, Stock = 25
+           //     },
+           // });
+
+
+           var p= await _productReadRepository.GetByIdAsync("53578154-d186-405a-bf31-61365e18db7a",false);
+           p.Name = "mehmet";
 
             _productWriteRepository.SaveAsync();
         }
